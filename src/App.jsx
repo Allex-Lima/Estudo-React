41ca4f8caa1b2c3f3a03deca486915ac79464409
Alexandro <alexlmangabeira@gmail.com>
2021-11-15 20:40:55 -0300

    Estado do Componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { RepositoryList } from './components/RepositoryList.jsx'
+import { Counter } from './components/Counter.jsx'
 import './styles/global.scss'
 
 export function App() {
     return (
-        <RepositoryList />
+        <>
+            <RepositoryList />
+            <Counter />
+        </>
     )
 }
